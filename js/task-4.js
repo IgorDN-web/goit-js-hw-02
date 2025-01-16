561bf87946140aaa07c4fdbbabd13e9cf7ee5987
IgorD <avtosklokyiv@gmail.com>
2025-01-16 20:22:48 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,15 +1,30 @@
-// Оголошуємо функцію checkForSpam
-function checkForSpam(message) {
-  // Перевірка на наявність слів 'spam' або 'sale' (у будь-якому регістрі)
-  const normalizedMessage = message.toLowerCase(); // Перетворюємо текст у нижній регістр
-  return normalizedMessage.includes("spam") || normalizedMessage.includes("sale");
+function getShippingCost(country) {
+  let shippingCost;
+
+  // Оператор switch для определения стоимости доставки
+  switch (country.toLowerCase()) {
+    case 'usa':
+      shippingCost = 10;
+      break;
+    case 'canada':
+      shippingCost = 15;
+      break;
+    case 'uk':
+      shippingCost = 20;
+      break;
+    case 'australia':
+      shippingCost = 25;
+      break;
+    default:
+      shippingCost = 30; // Стоимость для других стран
+  }
+
+  return shippingCost;
 }
 
-// Код для перевірки функціоналу
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+// Проверка функции с разными странами
+console.log(getShippingCost("USA")); // 10
+console.log(getShippingCost("Canada")); // 15
+console.log(getShippingCost("UK")); // 20
+console.log(getShippingCost("Australia")); // 25
+console.log(getShippingCost("Germany")); // 30
